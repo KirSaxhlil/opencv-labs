3e97ab5ec238640ea17eefed2a6bbb3dd04e2749
KirSaxhlil <dimapisarenko28@gmail.com>
2023-12-16 08:35:42 +0300

    Lab6 чекаем че получилось

--- Lab6/main.py
+++ Lab6/main.py
@@ -46,3 +46,30 @@ for epochs in epochs_list:
     results.append((epochs, round(training_time, 4)))
 
     model.save(f'models/mlp_model{epochs}.keras')
+
+for epochs,training_time in results:
+    print(f"Эпох: {epochs} "
+          f"Скорость обучения:{training_time}")
+
+results=[]
+for epochs in epochs_list:
+    model = load_model(f'models/mlp_model{epochs}.keras')
+
+    _,test_accuracy = model.evaluate(test_images, test_labels)
+
+    start_time = time.time()
+    predictions = model.predict(test_images)
+    end_time = time.time()
+    work_time=end_time-start_time
+
+    results.append((epochs, round(test_accuracy*100,2),round(work_time,4)))
+
+    index = np.random.randint(0, len(test_images))
+
+    plt.imshow(test_images[index].reshape(28, 28), cmap='gray')
+    plt.title(f'Предсказание модели: {np.argmax(predictions[index])}')
+    plt.show()
+
+for epochs, accuracy,work_time in results:
+    print(f"Эпох: {epochs}, Процент корректной работы на тестовых данных: {accuracy}%, "
+          f" Скорость работы сети:{work_time}")
